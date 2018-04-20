13943c54b1b2044d3db4f1f516933e91bbad7f88
dingmouren <1341520108@qq.com>
2018-04-20 18:55:18 +0800

    commit

--- app/src/main/java/com/dingmouren/mychat/ui/search/SearchActivity.java
+++ app/src/main/java/com/dingmouren/mychat/ui/search/SearchActivity.java
@@ -2,8 +2,10 @@ package com.dingmouren.mychat.ui.search;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.dingmouren.mychat.R;
@@ -17,7 +19,6 @@ public class SearchActivity extends AppCompatActivity {
     private static final String TAG = "SearchActivity";
 
     private ImageView mImgArrowBack;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
